b440867d4a994a1dc3115d90bcae2410b2cb62db
ryanbr <mp3geek@gmail.com>
2026-07-27 09:31:44 +0000

    Release 9.0.1-tan: bump version (none) + build numbers + What's New

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.noop.tan"
         minSdk = 26
         targetSdk = 34
-        versionCode = 296
-        versionName = "9.0.1-rc3-tan"
+        versionCode = 297
+        versionName = "9.0.1-tan"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
